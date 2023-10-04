0984db054f36db818bc605d05b2f008ac0740dfe
patrickleon710 <134448809+patrickleon710@users.noreply.github.com>
2023-10-04 22:22:13 +0300

    FIRST C CODE

---
+++ Hello World/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int index(void ) {
+    int a, b, c;
+    printf("Enter the first value :");
+    scanf("%d" , &a);
+    printf("Enter the second value :");
+    scanf("%d" , &a);
+    c = a + b;
+    printf("%d + %d = %d\n", a, b,c);
+    return 0;
+}
+
+
